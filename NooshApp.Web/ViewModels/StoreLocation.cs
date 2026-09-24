832bb3be0c6678249097bec5bf70706fd904844d
ST10441678 <ST10441678@vcconnect.edu.za>
2026-09-24 23:39:10 +0200

    The StoreLocation.cs file has been updated, the StoreLocation variables have now been added to the file, as well as the Directions url has also been stated.

--- NooshApp.Web/ViewModels/StoreLocation.cs
+++ NooshApp.Web/ViewModels/StoreLocation.cs
@@ -9,4 +9,23 @@ namespace NooshApp.Web.ViewModels
         public string Time { get; set; } = string.Empty;
         public bool IsClosure { get; set; } = false;
     }
+
+
+    public class StoreLocation
+    {
+        public string Name { get; set; } = string.Empty;
+        public string MallName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string WhatsAppNumber { get; set; } = string.Empty;
+        public List<StoreHoursLine> HoursSchedule { get; set; } = new();
+        public string MapEmbedUrl { get; set; } = string.Empty;
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public string UberEatsUrl { get; set; } = string.Empty;
+        public string MrDUrl { get; set; } = string.Empty;
+
+        public string DirectionsUrl =>
+            $"https://www.google.com/maps/dir/?api=1&destination={Uri.EscapeDataString(Address)}";
+    }
 }
