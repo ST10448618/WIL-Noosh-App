55bb895171c063c50f54684a99a0fe9bfce53973
ST10441678 <ST10441678@vcconnect.edu.za>
2026-09-25 23:41:07 +0200

    The CateringApiClient file has been updated, more variables and content has been added to the code for the cv file

--- NooshApp.Web/Services/Interfaces/CareersApiClient.cs
+++ NooshApp.Web/Services/Interfaces/CareersApiClient.cs
@@ -22,6 +22,26 @@ namespace NooshApp.Web.Services
             content.Add(new StringContent(model.Email), "email");
             content.Add(new StringContent(model.DesiredPosition), "desiredPosition");
             content.Add(new StringContent(model.CoverLetter ?? string.Empty), "coverLetter");
+        
+         using var cvStream = model.CvFile.OpenReadStream();
+            using var cvContent = new StreamContent(cvStream);
+            cvContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(model.CvFile.ContentType);
+            content.Add(cvContent, "cvFile", model.CvFile.FileName);
+
+            var docStreams = new List<Stream>(); // kept alive until the request completes
+
+            if (model.SupportingDocuments != null)
+            {
+                foreach (var doc in model.SupportingDocuments.Where(d => d.Length > 0))
+                {
+                    var docStream = doc.OpenReadStream();
+                    docStreams.Add(docStream);
+                    var docContent = new StreamContent(docStream);
+                    docContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(doc.ContentType);
+                    content.Add(docContent, "supportingDocuments", doc.FileName);
+                }
+            }
+        
         }
     }
 }
