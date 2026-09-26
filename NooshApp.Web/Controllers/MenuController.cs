9ee8aea8085a78a842a47941a646dc39ccb7b3ed
ST10441678 <ST10441678@vcconnect.edu.za>
2026-09-26 16:38:25 +0200

    The MenuController.cs file has been updated. The Index method to display all the items and categories have been added.

--- NooshApp.Web/Controllers/MenuController.cs
+++ NooshApp.Web/Controllers/MenuController.cs
@@ -10,10 +10,17 @@ namespace NooshApp.Web.Controllers
         public MenuController(IMenuApiClient menuApiClient) { _menuApiClient = menuApiClient; }
     
     
-    
-    
-    
-    
-    
+       public async Task<IActionResult> Index()
+        {
+            var items = await _menuApiClient.GetAllAsync();
+
+            var viewModel = new MenuViewModel
+            {
+                Items = items,
+                Categories = items.Select(i => i.Category).Distinct().OrderBy(c => c).ToList()
+            };
+
+            return View(viewModel);
+        }
     }
 }
