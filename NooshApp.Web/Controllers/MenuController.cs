ac3c4275ad0205849f58e78791957c3685c795ab
ST10441678 <ST10441678@vcconnect.edu.za>
2026-09-26 16:26:53 +0200

    The MenuController.cs file has been added to the controllers folder. The private and public declarations have been added.

---
+++ NooshApp.Web/Controllers/MenuController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using NooshApp.Web.Services;
+using NooshApp.Web.ViewModels;
+
+namespace NooshApp.Web.Controllers
+{
+    public class MenuController : Controller
+    {
+        private readonly IMenuApiClient _menuApiClient;
+        public MenuController(IMenuApiClient menuApiClient) { _menuApiClient = menuApiClient; }
+    
+    
+    
+    
+    
+    
+    
+    }
+}
--- NooshApp.Web/obj/Debug/net10.0/NooshApp.Web.AssemblyInfo.cs
+++ NooshApp.Web/obj/Debug/net10.0/NooshApp.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NooshApp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d51ff6e4b11b0b0fe1b50732c2becfb080c5bcde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+782f54530ce66c8319fb8a2795842de08d94b183")]
 [assembly: System.Reflection.AssemblyProductAttribute("NooshApp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NooshApp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
